3b48d34e9984f8a479e918f636be304924f73ed3
xTrayambak <xtrayambak@gmail.com>
2023-07-15 14:48:04 +0530

    (xxx) update tests for RNG.choice()

--- tests/test1.nim
+++ tests/test1.nim
@@ -4,5 +4,6 @@ import librng
 test "rng":
   var rng = newRNG()
   echo $rng
-  for _ in 0..32:
-    echo $rng.randint(range = 32, genAlgo = rngLCG)
+  let fruits = @["apples", "oranges", "pineapples", "mangoes", "bananas"]
+
+  echo "This is a fruit: " & rng.choice(fruits)
