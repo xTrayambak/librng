cbeaec8e4dce968b172f7270eb7295876e815b0f
xTrayambak <xtrayambak@gmail.com>
2023-07-15 14:47:15 +0530

    (fix) new exceptions

--- src/librng/generator.nim
+++ src/librng/generator.nim
@@ -1,6 +1,9 @@
-type Generator* = ref object of RootObj
- seed*: uint64 # for single state algorithms like Splitmix64
- state*: array[0..1, uint64] # for multi-state algorithms like Xoroshiro128
+type
+  GeneratorWorkInProgressDefect* = Defect
+  UninitializedGeneratorDefect* = Defect
+  Generator* = ref object of RootObj
+   seed*: uint64 # for single state algorithms like Splitmix64
+   state*: array[0..1, uint64] # for multi-state algorithms like Xoroshiro128
 
-method next*(generator: Generator): uint64 {.base.} =
+method next*(generator: Generator): uint64 {.base inline.} =
  0
