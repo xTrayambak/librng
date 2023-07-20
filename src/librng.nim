1e29a1be591f90705d3f95b055d0dd2cf60fe038
xTrayambak <xtrayambak@gmail.com>
2023-07-20 20:47:09 +0530

    (fix) fix indentation in src/librng.nim

--- src/librng.nim
+++ src/librng.nim
@@ -17,56 +17,56 @@ proc `$`*(rng: RNG): string =
   fmt"librng generator (seed: {rng.seed}; loaded generators: {rng.generators.len})"
 
 proc gen(rng: RNG, genAlgo: RNGAlgorithm): uint64 =
-  if genAlgo in rng.generators:
-    rng.generators[genAlgo].next()
-  else:
-    var gen: Generator
+ if genAlgo in rng.generators:
+  rng.generators[genAlgo].next()
+ else:
+  var gen: Generator
 
-    if genAlgo == rngSplitmix64:
-      gen = newSplitmix64(rng.seed)
-    elif genAlgo == rngXoroshiro:
-      gen = newXoroshiro128(
-        rng.gen(rngSplitmix64),
-        rng.gen(rngSplitmix64)
-      )
-    elif genAlgo == rngLCG:
-      gen = newLinearCongruentialGenerator(
-        rng.gen(rngSplitmix64),
-        rng.gen(rngSplitmix64),
-        rng.gen(rngSplitmix64),
-        rng.gen(rngSplitmix64)
-      )
-    elif genAlgo == rngMersenneTwister:
-      gen = newMersenneTwister(
-        rng.gen(rngSplitmix64)
-      )
+  if genAlgo == rngSplitmix64:
+   gen = newSplitmix64(rng.seed)
+  elif genAlgo == rngXoroshiro:
+    gen = newXoroshiro128(
+      rng.gen(rngSplitmix64),
+      rng.gen(rngSplitmix64)
+    )
+  elif genAlgo == rngLCG:
+   gen = newLinearCongruentialGenerator(
+    rng.gen(rngSplitmix64),
+    rng.gen(rngSplitmix64),
+    rng.gen(rngSplitmix64),
+    rng.gen(rngSplitmix64)
+   )
+  elif genAlgo == rngMersenneTwister:
+   gen = newMersenneTwister(
+    rng.gen(rngSplitmix64)
+   )
 
-    rng.generators[genAlgo] = gen
-    rng.gen(genAlgo)
+  rng.generators[genAlgo] = gen
+  rng.gen(genAlgo)
 
 proc randint*(rng: RNG, range: int = int.high, 
               genAlgo: RNGAlgorithm = rngXoroshiro
             ): int =
-  # Article on generating random numbers within a range
-  # https://www.pcg-random.org/posts/bounded-rands.html
-  cast[int](rng.gen(genAlgo)) mod range
+ # Article on generating random numbers within a range
+ # https://www.pcg-random.org/posts/bounded-rands.html
+ cast[int](rng.gen(genAlgo)) mod range
 
 proc choice*[T](rng: RNG, arr: seq[T], 
   genAlgo: RNGAlgorithm = rngXoroshiro
-  ): T =
-  var idx = rng.randint(arr.len, genAlgo)
+ ): T =
+ var idx = rng.randint(arr.len, genAlgo)
 
-  # This is a bad way of doing this!
-  while idx < 0:
-    idx = rng.randint(arr.len, genAlgo)
+ # This is a bad way of doing this!
+ while idx < 0:
+  idx = rng.randint(arr.len, genAlgo)
 
-  arr[idx]
+ arr[idx]
 
 proc newRNG*(seed: uint64 = 0): RNG =
-  var realSeed: uint64 = seed
-  if seed == 0:
-    realSeed = now().second.uint64 + now().minute.uint64
+ var realSeed: uint64 = seed
+ if seed == 0:
+  realSeed = now().second.uint64 + now().minute.uint64
 
-  RNG(seed: realSeed, generators: newTable[RNGAlgorithm, Generator]())
+ RNG(seed: realSeed, generators: newTable[RNGAlgorithm, Generator]())
 
 export RNGAlgorithm, RNG
