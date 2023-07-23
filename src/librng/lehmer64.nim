5b7ca3155fe6e34ddd9038eb7bdc2f5fd48fabb0
xTrayambak <xtrayambak@gmail.com>
2023-07-23 12:14:39 +0530

    (add) add lehmer64

---
+++ src/librng/lehmer64.nim
@@ -0,0 +1,14 @@
+import generator
+
+type Lehmer64* = ref object of Generator
+
+method next*(lehmer64: Lehmer64): uint64 =
+ lehmer64.seed *= 0xda942042e4dd58b5'u64
+ let val = lehmer64.seed shr 64
+
+ lehmer64.seed = val
+
+ val
+
+proc newLehmer64*(seed: uint64): Lehmer64 =
+ Lehmer64(seed: seed)
