64b82325d16a63aa11f65fe2e72f9dc694f995fe
xTrayambak <xtrayambak@gmail.com>
2024-01-31 20:59:09 +0530

    (add) add Splitmix64 to multi algo example

--- examples/multi_algo.nim
+++ examples/multi_algo.nim
@@ -5,14 +5,18 @@ var
   lehmer64 = newRNG(algo = Lehmer64)
   mersenne = newRNG(algo = MersenneTwister)
   marsaglia = newRNG(algo = Marsaglia69069)
+  lcg = newRNG(algo = LCG)
+  smix64 = newRNG(algo = Splitmix64)
 
 const
   libraries = [
     "pixie", "fidget", "chame", "nimlsp", "jsony", "librng", "gintro", "owlkettle",
-    "nimcrypto"
+    "nimcrypto", "netty", "puppy", "happyx", "jester", "httpbeast", "modernnet"
   ]
 
 echo "Xoroshiro128 likes: " & xoroshiro128.choice(libraries)
 echo "Lehmer64 likes: " & lehmer64.choice(libraries)
 echo "Mersenne Twister likes: " & mersenne.choice(libraries)
 echo "Marsaglia69069 likes: " & marsaglia.choice(libraries)
+echo "LCG likes: " & lcg.choice(libraries)
+echo "Splitmix64 likes: " & smix64.choice(libraries)
