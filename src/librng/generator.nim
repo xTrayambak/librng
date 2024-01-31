b26f7d29377541b1dd1a36cfe45af78f0f31b235
xTrayambak <xtrayambak@gmail.com>
2024-01-31 20:59:26 +0530

    (fix) make defects inherit from , not

--- src/librng/generator.nim
+++ src/librng/generator.nim
@@ -1,6 +1,8 @@
+## The base class from which all PRNG algorithms derive themselves from. It simply implements a `next` function as a base.
+
 type
-  GeneratorWorkInProgressDefect* = Defect
-  UninitializedGeneratorDefect* = Defect
+  GeneratorWorkInProgressDefect* = object of Defect
+  UninitializedGeneratorDefect* = object of Defect
   Generator* = ref object of RootObj
     seed*: uint64 # for single state algorithms like Splitmix64
     state*: array[0..64, uint64] # for multi-state algorithms like Xoroshiro128
@@ -8,4 +10,4 @@ type
 method next*(generator: Generator): uint64 {.base, inline.} =
   raise newException(
       UninitializedGeneratorDefect, "Generator does not implement a next() function"
-    )
+  )
